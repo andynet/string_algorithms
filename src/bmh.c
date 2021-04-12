bbeeaefb2f73cb633bc6dc33f735ba2d5c72e66a
andynet <andrejbalaz001@gmail.com>
2021-04-12 22:10:58 +0200

    implemented Boyer-Moore-Horspool algorithm from Ohlebusch

--- src/bmh.c
+++ src/bmh.c
@@ -1,27 +1,36 @@
 #include <string.h>
+#include <stdlib.h>
+
+int *precompute_lambda(char *pattern){
+    size_t m = strlen(pattern);
+    int *lambda = (int *)calloc(256, (sizeof *lambda));
+    for (size_t i = 0; i < m; i++){
+        lambda[pattern[i]] = i;
+    }
+    return lambda;
+}
 
 int bmh(char *pattern, char *text) {
-    int match = 0;
+    int *lambda = precompute_lambda(pattern);
+    int n = (int)strlen(text);
+    int m = (int)strlen(pattern);
     static int j = -1;
     j++;
-    for (; j < strlen(text); j++) {
-        for (int i = 0; i < strlen(pattern); i++) {
-            if (pattern[i] == text[j + match]) {
-                match++;
-            } else {
-                match = 0;
-                break;
-            }
-            if (match == strlen(pattern)) {
-                return j;
-            }
+    while (j <= n - m){
+        int i = m - 1;
+        while (i >= 0 && pattern[i] == text[j+i]){
+            i--;
         }
+        if (i < 0){
+            return j + i + 1;
+        }
+        j = j + m - lambda[text[j+m]];
     }
     return -1;
 }
 
 //int main(void) {
-//    int m = bmh("AB", "BBBBAB");
+//    int m = bmh("AB", "ABBBB");
 //    return 0;
 //}
 
