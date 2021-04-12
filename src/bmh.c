0e2e27922600ae0a6c3c81f70ec95b5b9a74b8bb
andynet <andrejbalaz001@gmail.com>
2021-04-12 21:29:50 +0200

    tests finished

--- src/bmh.c
+++ src/bmh.c
@@ -1,2 +1,27 @@
+#include <string.h>
 
+int bmh(char *pattern, char *text) {
+    int match = 0;
+    static int j = -1;
+    j++;
+    for (; j < strlen(text); j++) {
+        for (int i = 0; i < strlen(pattern); i++) {
+            if (pattern[i] == text[j + match]) {
+                match++;
+            } else {
+                match = 0;
+                break;
+            }
+            if (match == strlen(pattern)) {
+                return j;
+            }
+        }
+    }
+    return -1;
+}
+
+//int main(void) {
+//    int m = bmh("AB", "BBBBAB");
+//    return 0;
+//}
 
--- test/test_bmh.c
+++ test/test_bmh.c
@@ -4,6 +4,32 @@
 
 START_TEST(empty_test){} END_TEST
 
+START_TEST(no_match){
+    ck_assert_int_eq(bmh("A", "BBB"), -1);
+} END_TEST
+
+START_TEST(match_at_the_beginning){
+    ck_assert_int_eq(bmh("AB", "ABBBB"), 0);
+} END_TEST
+
+START_TEST(match_at_the_end){
+    ck_assert_int_eq(bmh("AB", "BBBBAB"), 4);
+} END_TEST
+
+START_TEST(match_in_the_middle){
+    ck_assert_int_eq(bmh("AB", "BBBABBBB"), 3);
+} END_TEST
+
+START_TEST(multiple_matches){
+    ck_assert_int_eq(bmh("AB", "ABBAB"), 0);
+    ck_assert_int_eq(bmh("AB", "ABBAB"), 3);
+} END_TEST
+
+START_TEST(multiple_overlapping_matches){
+    ck_assert_int_eq(bmh("AAA", "AAAA"), 0);
+    ck_assert_int_eq(bmh("AAA", "AAAA"), 1);
+} END_TEST
+
 Suite * test_suite(void){
     Suite *suite;
     TCase *core;
@@ -11,6 +37,12 @@ Suite * test_suite(void){
     suite = suite_create("test");
     core = tcase_create("core");
     tcase_add_test(core, empty_test);
+    tcase_add_test(core, no_match);
+    tcase_add_test(core, match_at_the_beginning);
+    tcase_add_test(core, match_at_the_end);
+    tcase_add_test(core, match_in_the_middle);
+    tcase_add_test(core, multiple_matches);
+    tcase_add_test(core, multiple_overlapping_matches);
     suite_add_tcase(suite, core);
     return (suite);
 }
